43a792469a5d86c89246c91df0d78c62d7d2ab3d
Ru Tan <jutan2012@gmail.com>
2016-12-21 16:35:43 +0100

    etape 2 avec majuscule en JS

--- public/app.js
+++ public/app.js
@@ -1,24 +1,30 @@
 'use strict';
 
-//function hello(person = 'Ju'){
-//	console.log('Bonjour' + person);
-//}
-//hello('Père Noel');
-//hello();
-var input = document.getElementById('info').value;
 var button = document.getElementById('valider');
+var buttonSearch = document.getElementById('search');
 var output = document.getElementById('infoAfficher');
 var tab = [];
 button.addEventListener('click', function (event) {
-	console.log('marche');
 	var input = document.getElementById('info').value;
+
 	var ul = document.getElementById('list');
-	var li = document.createElement('li');
-	ul.appendChild(li);
-	li.innerHTML = input;
+
 	if (input === "") {
-		li.innerHTML = 'Père Noel ! ';
+		output.innerHTML = 'Bonjour Père Noël !';
+	} else {
+		var tableau = input;
+		tab.push(input);
+		var premier = tableau.charAt(0).toUpperCase();
+		var majuscul = premier + input.slice(1).toLowerCase();
+		var outputValue = document.getElementById('infoAfficher').value;
+		document.getElementById('info').innerHTML = majuscul;
+		document.getElementById('infoAfficher').appendChild(document.createElement('li')).innerHTML = majuscul;
+
+		ul.innerHTML = 'Bonjour' + majuscul;
+
+		console.log(tab);
 	}
-	tab.push(input);
-	console.log(tab);
+});
+buttonSearch.addEventListener('click', function (event) {
+	var inputSearch = document.getElementById('infoSearch').value;
 });
